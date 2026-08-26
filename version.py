81d078963492196dba3090ed0796af952f2ac35d
LukaStuurman <32568576+LukaStuurman@users.noreply.github.com>
2026-08-26 10:35:21 +0200

    Set SleufBase version to 0.3.0

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """SleufBase product version information."""
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 PRODUCT_NAME = "SleufBase"
 COMPANY_NAME = "Techbase"
 APP_USER_MODEL_ID = "Techbase.SleufBase"
