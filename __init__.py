2002c35b78ef5645949388be6a3ee1d86b24b1a7
LukaStuurman <32568576+LukaStuurman@users.noreply.github.com>
2026-08-28 11:41:08 +0200

    Install Dynamic Block finalizer

--- __init__.py
+++ __init__.py
@@ -2,7 +2,9 @@
 
 from .settings_ui import install_settings_ui_patch
 from .settings_general_layout_patch import install_settings_general_layout_patch
+from .dynamic_visibility_finalize_patch import install_dynamic_visibility_finalize_patch
 
 
 install_settings_ui_patch()
 install_settings_general_layout_patch()
+install_dynamic_visibility_finalize_patch()
