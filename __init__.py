df6d0ebba6ca0ac20d5a0e003707af398bcf548d
LukaStuurman <32568576+LukaStuurman@users.noreply.github.com>
2026-08-28 13:26:25 +0200

    Installeer uitgebreide KickTheMap- en autosave-instellingen

--- __init__.py
+++ __init__.py
@@ -2,11 +2,15 @@
 
 from .settings_ui import install_settings_ui_patch
 from .settings_general_layout_patch import install_settings_general_layout_patch
+from .settings_kickthemap_autosave_patch import (
+    install_settings_kickthemap_autosave_patch,
+)
 from .dynamic_visibility_finalize_patch import install_dynamic_visibility_finalize_patch
 from .template_dynamic_profile_leader_patch import install_dynamic_profile_leader_patch
 
 
 install_settings_ui_patch()
 install_settings_general_layout_patch()
+install_settings_kickthemap_autosave_patch()
 install_dynamic_visibility_finalize_patch()
 install_dynamic_profile_leader_patch()
