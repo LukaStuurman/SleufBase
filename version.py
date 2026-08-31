8416d16f7ac6422d9d6a5b4fb361a89c1dfa02c8
LukaStuurman <32568576+LukaStuurman@users.noreply.github.com>
2026-08-31 12:45:37 +0200

    Prepare Windows release v0.3.16

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """SleufBase product version information."""
 
-__version__ = "0.3.15"
+__version__ = "0.3.16"
 PRODUCT_NAME = "SleufBase"
 COMPANY_NAME = "Techbase"
 APP_USER_MODEL_ID = "Techbase.SleufBase"
