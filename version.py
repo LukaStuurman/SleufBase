c28bae44e509b06db926e03dec601d53958309b3
LukaStuurman <32568576+LukaStuurman@users.noreply.github.com>
2026-08-31 08:45:10 +0200

    Prepare SleufBase v0.3.13

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """SleufBase product version information."""
 
-__version__ = "0.3.12"
+__version__ = "0.3.13"
 PRODUCT_NAME = "SleufBase"
 COMPANY_NAME = "Techbase"
 APP_USER_MODEL_ID = "Techbase.SleufBase"
