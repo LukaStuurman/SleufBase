8cdb5c178c8c392ae500e94fa651708a18cfc89f
LukaStuurman <32568576+LukaStuurman@users.noreply.github.com>
2026-09-03 15:26:43 +0200

    Bump SleufBase naar 0.3.34

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """SleufBase product version information."""
 
-__version__ = "0.3.33"
+__version__ = "0.3.34"
 PRODUCT_NAME = "SleufBase"
 COMPANY_NAME = "Techbase"
 APP_USER_MODEL_ID = "Techbase.SleufBase"
