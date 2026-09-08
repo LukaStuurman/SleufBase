943085818f8ed226206484e6aa5331449693b106
LukaStuurman <32568576+LukaStuurman@users.noreply.github.com>
2026-09-08 19:12:13 +0200

    Wire live DXF pipeline predicate bridge

--- __init__.py
+++ __init__.py
@@ -20,6 +20,7 @@ from .render_cache_performance_patch import install_render_cache_performance_pat
 from .marxact_live_render_patch import install_marxact_live_render_patch
 from .marxact_alignment_ui_patch import install_marxact_alignment_ui_hook
 from .dxf_template_pipeline_patch import install_dxf_template_pipeline_patch
+from .dxf_template_pipeline_bridge import install_dxf_template_pipeline_bridge
 
 
 install_settings_ui_patch()
@@ -40,3 +41,4 @@ install_render_cache_performance_patch()
 install_marxact_live_render_patch()
 install_marxact_alignment_ui_hook()
 install_dxf_template_pipeline_patch()
+install_dxf_template_pipeline_bridge()
