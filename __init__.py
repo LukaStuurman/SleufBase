65e8c9d4a3ed2ec87e6ba0cdcb3d32d026c9734f
LukaStuurman <32568576+LukaStuurman@users.noreply.github.com>
2026-09-09 10:55:11 +0200

    Activeer v6 veilige reverse profielrebuild

--- __init__.py
+++ __init__.py
@@ -24,6 +24,7 @@ from .dxf_template_pipeline_bridge import install_dxf_template_pipeline_bridge
 from .dxf_template_pipeline_v3_patch import install_dxf_template_pipeline_v3_patch
 from .dxf_template_pipeline_v4_patch import install_dxf_template_pipeline_v4_patch
 from .dxf_template_pipeline_v5_patch import install_dxf_template_pipeline_v5_patch
+from .dxf_template_pipeline_v6_patch import install_dxf_template_pipeline_v6_patch
 
 
 install_settings_ui_patch()
@@ -48,3 +49,4 @@ install_dxf_template_pipeline_bridge()
 install_dxf_template_pipeline_v3_patch()
 install_dxf_template_pipeline_v4_patch()
 install_dxf_template_pipeline_v5_patch()
+install_dxf_template_pipeline_v6_patch()
