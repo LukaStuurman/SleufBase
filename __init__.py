a9703f1b6d15422c0cd5cded458a6bccf7e3bea2
LukaStuurman <32568576+LukaStuurman@users.noreply.github.com>
2026-09-09 15:26:34 +0200

    Cleanup: remove absorbed render cache installer

--- __init__.py
+++ __init__.py
@@ -16,7 +16,6 @@ from .pdok_high_quality_patch import install_pdok_high_quality_patch
 from .marxact_boundary_patch import install_marxact_boundary_patch
 from .marxact_local_cross_section_patch import install_marxact_local_cross_section_patch
 from .render_hotpath_patch import install_render_hotpath_patch
-from .render_cache_performance_patch import install_render_cache_performance_patch
 from .render_framebuffer_performance_patch import install_render_framebuffer_performance_patch
 from .marxact_live_render_patch import install_marxact_live_render_patch
 from .marxact_alignment_ui_patch import install_marxact_alignment_ui_hook
@@ -43,7 +42,6 @@ install_pdok_high_quality_patch()
 install_marxact_boundary_patch()
 install_marxact_local_cross_section_patch()
 install_render_hotpath_patch()
-install_render_cache_performance_patch()
 install_render_framebuffer_performance_patch()
 install_marxact_live_render_patch()
 install_marxact_alignment_ui_hook()
